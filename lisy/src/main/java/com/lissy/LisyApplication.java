d63753dd2d1cb8538e11055e231b917f43b9d0ec
Omar Hernandez <Omar Hernandez>
2016-12-11 16:50:38 -0600

    Proyecto lissy Inicializado

---
+++ lisy/src/main/java/com/lissy/LisyApplication.java
@@ -0,0 +1,12 @@
+package com.lissy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LisyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LisyApplication.class, args);
+	}
+}
